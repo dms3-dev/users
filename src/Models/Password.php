9f3842bef07654ca222c50bb376987e23093a292
Sjoerd Steeman <sjoerd@mediamouse.nl>
2023-02-03 12:41:24 +0100

    Created the users migrations

---
+++ src/Models/Password.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mediamouse\Users\Models;
+
+use Carbon\Carbon;
+use Mediamouse\Laravel\Models\Model;
+
+/**
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property string password
+ */
+
+class Password extends Model
+{
+
+}
---
+++ src/Models/PasswordReset.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mediamouse\Users\Models;
+
+use Carbon\Carbon;
+use Mediamouse\Laravel\Models\Model;
+
+/**
+ * @property Carbon created_at
+ * @property string token
+ * @property string status
+ */
+
+class PasswordReset extends Model
+{
+
+}
