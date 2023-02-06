2cfe81a10ef0aed8d478b3585fb2995892495f66
Sjoerd Steeman <sjoerd@mediamouse.nl>
2023-02-06 09:16:18 +0100

    Moved Create, Edit and ListUsers to the package

---
+++ src/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mediamouse\Users\Filament\Resources\UserResource\Pages;
+
+use Mediamouse\Users\Filament\Resources\UserResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+}
---
+++ src/Filament/Resources/UserResource/Pages/EditUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mediamouse\Users\Filament\Resources\UserResource\Pages;
+
+use Mediamouse\Users\Filament\Resources\UserResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUser extends EditRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
