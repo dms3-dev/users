08ac0c5af9cf550561ae1f960fcea7aaf7135768
Jeroen de Leur <info@jeroendeleur.nl>
2023-02-27 10:07:04 +0100

    Updated policy

--- src/Policies/PolicyAbstract.php
+++ src/Policies/PolicyAbstract.php
@@ -30,7 +30,6 @@ abstract class PolicyAbstract {
             return $model->userIsAllowed($user, $this->type(), PolicyPrivilege::VIEW);
         }
         return $user->isAllowed($this->type(), PolicyPrivilege::VIEW);
-
     }
 
     /**
@@ -39,7 +38,6 @@ abstract class PolicyAbstract {
     public function create(User $user): bool
     {
         return $user->isAllowed($this->type(), PolicyPrivilege::CREATE);
-
     }
 
     /**
@@ -51,7 +49,6 @@ abstract class PolicyAbstract {
             return $model->userIsAllowed($user, $this->type(), PolicyPrivilege::UPDATE);
         }
         return $user->isAllowed($this->type(), PolicyPrivilege::UPDATE);
-
     }
 
     /**
@@ -63,7 +60,6 @@ abstract class PolicyAbstract {
             return $model->userIsAllowed($user, $this->type(), PolicyPrivilege::DELETE);
         }
         return $user->isAllowed($this->type(), PolicyPrivilege::DELETE);
-
     }
 
     /**
@@ -75,7 +71,6 @@ abstract class PolicyAbstract {
             return $model->userIsAllowed($user, $this->type(), PolicyPrivilege::RESTORE);
         }
         return $user->isAllowed($this->type(), PolicyPrivilege::RESTORE);
-
     }
 
     /**
@@ -87,6 +82,5 @@ abstract class PolicyAbstract {
             return $model->userIsAllowed($user, $this->type(), PolicyPrivilege::FORCE_DELETE);
         }
         return $user->isAllowed($this->type(), PolicyPrivilege::FORCE_DELETE);
-
     }
 }
