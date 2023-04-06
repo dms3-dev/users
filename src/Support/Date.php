2926cff3d1a367c44c22331617d8ec8b0345dc61
Jeroen de Leur <jeroen@mediamouse.nl>
2023-04-06 15:28:51 +0200

    Added support for globalFormat

--- src/Support/Date.php
+++ src/Support/Date.php
@@ -6,6 +6,16 @@ class Date {
 
     public static function format(): string
     {
+        return self::userFormat();
+    }
+
+    public static function userFormat(): string
+    {
+        return 'Y-m-d H:i:s';
+    }
+
+    public static function globalFormat(): string
+    {
         return 'Y-m-d H:i:s';
     }
 
