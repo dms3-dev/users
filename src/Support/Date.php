e35d14f55c8dee5c5a23f4895f9fa807c66770f1
Sjoerd Steeman <sjoerd@mediamouse.nl>
2023-04-13 09:21:12 +0200

    Added more date formats for none, global and user

--- src/Support/Date.php
+++ src/Support/Date.php
@@ -6,17 +6,61 @@ class Date {
 
     public static function format(): string
     {
-        return self::userFormat();
+        return self::dateTimeFormat();
+    }
+
+    public static function dateFormat(): string
+    {
+        return self::userDateFormat();
+    }
+
+    public static function timeFormat(): string
+    {
+        return self::userTimeFormat();
+    }
+
+    public static function dateTimeFormat(): string
+    {
+        return self::userDateTimeFormat();
     }
 
     public static function userFormat(): string
     {
-        return 'Y-m-d H:i:s';
+        return 'M d Y H:i:s';
+    }
+
+    public static function userDateFormat(): string
+    {
+        return 'M d Y';
+    }
+
+    public static function userTimeFormat(): string
+    {
+        return 'H:i:s';
+    }
+
+    public static function userDateTimeFormat(): string
+    {
+        return 'M d Y H:i:s';
     }
 
     public static function globalFormat(): string
     {
-        return 'Y-m-d H:i:s';
+        return 'M d Y H:i:s';
     }
 
+    public static function globalTimeFormat(): string
+    {
+        return 'H:i:s';
+    }
+
+    public static function globalDateFormat(): string
+    {
+        return 'M d Y';
+    }
+
+    public static function globalDateTimeFormat(): string
+    {
+        return 'M d Y H:i:s';
+    }
 }
