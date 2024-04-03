b8e85e02af5e14b4d6e7f7763e3fde81d8aeca9c
Jeroen de Leur <jeroen@mediamouse.nl>
2024-04-03 16:57:04 +0200

    Moved the policy of changelogs

---
+++ src/Policies/ChangeLogPolicy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mediamouse\Users\Policies;
+
+use Mediamouse\Users\Policies\PolicyAbstract;
+
+class ChangeLogPolicy extends PolicyAbstract
+{
+    protected string $name = 'Change log management';
+
+}
