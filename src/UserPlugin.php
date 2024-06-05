0582ff2a66731db192e6e73b7e7a085121267e89
Jeroen de Leur <jeroen@mediamouse.nl>
2024-06-05 11:35:16 +0200

    Update

--- src/UserPlugin.php
+++ src/UserPlugin.php
@@ -14,7 +14,7 @@ class UserPlugin implements Plugin
 {
     public function getId(): string
     {
-        return 'filament-apex-charts';
+        return 'mediamouse-users';
     }
 
     public static function make(): static
---
+++ src/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+function hasPrivilege(string $policy, \Mediamouse\Users\Enums\PolicyPrivilege $privilege) : bool {
+    /** @var \Mediamouse\Users\Models\User $user */
+    $user = \Filament\Facades\Filament::auth()->user();
+
+    return $user->hasPrivilege($policy, $privilege);
+}
