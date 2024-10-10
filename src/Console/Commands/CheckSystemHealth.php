c844e3e2cee52cf0268a42f5e9db3aaae819caea
AzureAD\GeorgiosKoutanis <georgios@mediamouse.nl>
2024-10-10 17:17:39 +0200

    now sends mails to Gijs Jeroen and George if a check changed to warning or error

--- src/Console/Commands/CheckSystemHealth.php
+++ src/Console/Commands/CheckSystemHealth.php
@@ -4,6 +4,8 @@ namespace Mediamouse\Users\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Mail;
+use Mediamouse\Users\Enums\SystemHealthStatus;
 use Mediamouse\Users\Models\Policy;
 use Mediamouse\Users\Models\Group;
 use Mediamouse\Users\Models\GroupHasPolicy;
@@ -26,17 +28,42 @@ class CheckSystemHealth extends Command
      */
     protected $description = 'Check System Health';
 
+    private array $errors;
+    private bool $changed = false;
+
     /**
      * Execute the console command.
      */
     public function handle() : int
     {
+        $this->errors = [];
         $this->withProgressBar($this->getChecks(), function(SystemHealth $check) {
+            $status = $check->status;
 
             $check->check($this->option('force'));
+
+            if($check->status !== $status && in_array($check->status, [
+                    SystemHealthStatus::ERROR,
+                    SystemHealthStatus::WARNING,
+                ])) {
+                $this->changed = true;
+            }
+            $this->mark($check->status, $check->health_check);
+
         });
 
+        if($this->changed) {
+            $this->sendErrorMail('Jeroen', 'jeroen@mediamouse.nl',  $this->errors);
+            $this->sendErrorMail('Gijs', 'gijs@mediamouse.nl',  $this->errors);
+            $this->sendErrorMail('George', 'georgios@mediamouse.nl',  $this->errors);
+
+        }
+
         return self::SUCCESS;
+    }
+
+    private function mark($status, $class) {
+        $this->errors[] = $status->value . ' ' . $class;
     }
 
     private function getChecks(): Collection|array
@@ -44,5 +71,22 @@ class CheckSystemHealth extends Command
         return SystemHealth::query()->get();
     }
 
+    public function sendErrorMail(string $name, string $email, $payload)
+    {
+        Mail::send([
+//            'html' => "Hey {$name} " . ' There is an error with ' . $class_name . ' on ' . env('APP_NAME'),
+            'raw'  => implode("\n", $payload),
+        ],[], function (\Illuminate\Mail\Message $message) use ($name, $email) {
+
+            $message
+                ->to($email)
+//                ->setBody(new TextPart("Hey {$name} " . ' There is an error with ' . $class_name . ' on ' . env('APP_NAME')), 'text/html')
+                ->subject('System health error list of' . env('APP_NAME'))
+//                ->addPart(new TextPart("Hey {$name} " . ' There is an error with ' . $class_name . ' on ' . env('APP_NAME')), 'text/plain')
+//            ->send()
+            ;
+        });
+
+    }
 
 }
