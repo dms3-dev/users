50c2646135b115776debb149c69be6252b20faf0
Jeroen de Leur <jeroen@mediamouse.nl>
2024-12-05 17:19:04 +0100

    Changed body of message

--- src/Console/Commands/CheckSystemHealth.php
+++ src/Console/Commands/CheckSystemHealth.php
@@ -73,7 +73,7 @@ class CheckSystemHealth extends Command
 
     public function sendErrorMail(string $name, string $email, $payload)
     {
-        Mail::to($email)->send(new SimpleMail('System health error list of ' . env('APP_NAME'), $email));
+        Mail::to($email)->send(new SimpleMail('System health error list of ' . env('APP_NAME'), implode("\r\n", $payload)));
     }
 
 }
