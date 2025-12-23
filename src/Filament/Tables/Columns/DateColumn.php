d692537bb296f998696ec932072898df4c5c86b2
Jeroen de Leur <jeroen@mediamouse.nl>
2025-12-23 10:45:55 +0100

    Fix for Filament 4

--- src/Filament/Tables/Columns/DateColumn.php
+++ src/Filament/Tables/Columns/DateColumn.php
@@ -11,7 +11,7 @@ use Mediamouse\Users\Support\Date;
 class DateColumn extends TextColumn
 {
 
-    public static function make(string $name): static
+    public static function make(?string $name = null): static
     {
         return parent::make($name)
             ->dateTime(Date::userDateFormat());
